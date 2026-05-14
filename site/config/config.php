a170a58fadd907049e90c4f42c4b5d95117b103f
Matleftod <97225801+Matleftod@users.noreply.github.com>
2026-05-14 17:02:38 +0200

    fix aide backoffice

--- site/config/config.php
+++ site/config/config.php
@@ -2,6 +2,18 @@
 
 return [
     'debug' => true,
+    'panel' => [
+        'menu' => [
+            'site',
+            'help' => [
+                'icon'  => 'question',
+                'label' => 'Aide',
+                'link'  => 'pages/aide',
+            ],
+            'users',
+            'system',
+        ],
+    ],
     'cache' => [
         'pages' => [
             'active' => false,
